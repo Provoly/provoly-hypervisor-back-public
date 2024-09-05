b6ca67ea95192b6f4febad19a73774c135df3cb5
j.lelong <j.lelong@groupeonepoint.com>
2024-09-05 09:31:53 +0200

    feat: Improve mock data loader

--- src/main/java/com/provoly/MockController.java
+++ src/main/java/com/provoly/MockController.java
@@ -1,22 +1,32 @@
 package com.provoly;
 
 import java.security.SecureRandom;
+import java.time.Duration;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import jakarta.transaction.Transactional;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
+import com.provoly.equipment.Equipment;
 import com.provoly.event.*;
 import com.provoly.model.ProcedureModel;
+import com.provoly.service.ServiceService;
+import com.provoly.service.ServiceStatus;
+import com.provoly.service.ServiceWriteDto;
 
 import io.quarkus.security.Authenticated;
 
+import org.jboss.logging.Logger;
 import org.jboss.resteasy.reactive.RestQuery;
 
 @Path("/mock")
@@ -25,49 +35,103 @@ import org.jboss.resteasy.reactive.RestQuery;
 public class MockController {
     private final Random rand = new SecureRandom();
 
+    private final Logger log;
+
     private final EntityManager entityManager;
     private final EventDatabaseReader databaseReader;
+    private final ServiceService serviceService;
+    private final EntityManager em;
 
-    public MockController(EntityManager entityManager, EventDatabaseReader databaseReader) {
+    public MockController(Logger log,
+            EntityManager entityManager,
+            EventDatabaseReader databaseReader,
+            ServiceService serviceService,
+            EntityManager em) {
+        this.log = log;
         this.entityManager = entityManager;
         this.databaseReader = databaseReader;
+        this.serviceService = serviceService;
+        this.em = em;
     }
 
     @POST
     @Authenticated
     @Transactional
     public void mock(@DefaultValue("30") @Positive @RestQuery int eventNumber,
-            @DefaultValue("10") @PositiveOrZero @RestQuery int procedureNumber) {
+            @DefaultValue("10") @PositiveOrZero @RestQuery int procedureNumber,
+            @DefaultValue("false") @RestQuery boolean withService) {
+
+        log.info("Populating database with sample results");
 
         var epDomain = databaseReader.getDomainByCode("EP");
         var vpDomain = databaseReader.getDomainByCode("VP");
+        var equipments = selectAllEquipment();
 
         var categories = databaseReader.getCategoryOrSubCategories();
 
         for (int i = 0; i < procedureNumber; i++) {
+            Domain domain = randomDomain(List.of(epDomain, vpDomain));
             var procedureModel = new ProcedureModel("modele de procédure n°%s".formatted(suffix(UUID.randomUUID())),
-                    "Agathe ThePower", "desc", randomDomain(List.of(epDomain, vpDomain)), List.of());
+                    "Agathe ThePower", "desc", domain, List.of());
             entityManager.persist(procedureModel);
         }
 
-        for (int i = 0; i <= eventNumber; i++) {
+        for (int i = 0; i < eventNumber; i++) {
             Event event = new Event();
             event.setCategory(randomCategory(categories));
-            event.setName("Evenement %s %s".formatted(event.getCategory().name, suffix(UUID.randomUUID())));
-            event.setCreationDate(Instant.now());
+            event.setName("MOCK: Evenement %s %s".formatted(event.getCategory().name, suffix(UUID.randomUUID())));
+            event.setCreationDate(randomCreationDate());
 
             if (event.getCategory().getCode().equals("MANIFESTATION")) {
-                event.setStartDate(Instant.now());
-                event.setEndDate(Instant.now().plus(rand.nextInt(1, 10), ChronoUnit.DAYS));
+                event.setStartDate(event.getCreationDate().plus(1, ChronoUnit.DAYS));
+                event.setEndDate(event.getCreationDate().plus(5, ChronoUnit.DAYS));
             }
             event.setAddress("%s rue de Chalons".formatted(i));
             event.setDescription("description of %s".formatted(event.getName()));
             event.setCriticality(randomCriticality());
-            event.setDomain(vpDomain);
+            event.setDomain(randomDomain(List.of(epDomain, vpDomain)));
+            event.setEquipment(randomEquipment(equipments));
             entityManager.persist(event);
             setStatus(event);
             setCloseDate(event);
+
+            if (withService) {
+                String serviceId = suffix(UUID.randomUUID());
+                Instant creationDate = event.getCreationDate().minus(3, ChronoUnit.DAYS);
+                Instant startDate = creationDate;
+                var service = new ServiceWriteDto(
+                        "Mock: service %s".formatted(serviceId),
+                        "description of service %s".formatted(serviceId),
+                        event.getEquipment().getName(),
+                        creationDate,
+                        creationDate.plus(5, ChronoUnit.DAYS),
+                        startDate,
+                        startDate.plus(3, ChronoUnit.DAYS),
+                        startDate.plus(5, ChronoUnit.DAYS),
+                        event.getDomain().getCode(),
+                        ServiceStatus.DONE,
+                        randomServiceCategory());
+                serviceService.saveOrUpdateServices(List.of(service));
+
+            }
         }
+    }
+
+    private Instant randomCreationDate() {
+        var start = Instant.now().minus(4 * 365, ChronoUnit.DAYS);
+        var end = Instant.now();
+        var durationNbSeconds = Duration.between(start, end).toSeconds();
+        var durationFromStart = Duration.of(rand.nextLong(durationNbSeconds), ChronoUnit.SECONDS);
+        return start.plus(durationFromStart);
+    }
+
+    private List<Equipment> selectAllEquipment() {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Equipment> cq = cb.createQuery(Equipment.class);
+        Root<Equipment> rootEntry = cq.from(Equipment.class);
+        CriteriaQuery<Equipment> all = cq.select(rootEntry);
+        TypedQuery<Equipment> allQuery = em.createQuery(all).setMaxResults(100);
+        return allQuery.getResultList();
     }
 
     private void setStatus(Event event) {
@@ -78,14 +142,10 @@ public class MockController {
 
     private void setCloseDate(Event event) {
         if (event.getStatus() == Status.DONE) {
-            event.setCloseDate(randomInstant());
+            var start = event.getCreationDate();
+            var duration = Duration.ofHours(rand.nextLong(5, 24 * 10));
+            event.setCloseDate(start.plus(duration));
         }
-    }
-
-    private Instant randomInstant() {
-        long date1 = 1704118449; // 1/1/24
-        long date2 = 1735654449; //31/12/24
-        return Instant.ofEpochSecond(rand.nextLong(date2 - date1) + date1);
     }
 
     private String suffix(UUID id) {
@@ -97,6 +157,11 @@ public class MockController {
         return values.get(rand.nextInt(values.size()));
     }
 
+    private String randomServiceCategory() {
+        var values = List.of("PREV", "CURA");
+        return values.get(rand.nextInt(values.size()));
+    }
+
     private Domain randomDomain(List<Domain> domains) {
         return domains.get(rand.nextInt(domains.size()));
     }
@@ -104,6 +169,10 @@ public class MockController {
     private Category randomCategory(Collection<Category> categories) {
         categories = categories.stream().filter(category -> !category.getCode().equals("ANOMALY")).toList();
         return categories.stream().toList().get(rand.nextInt(categories.size()));
+    }
+
+    private Equipment randomEquipment(List<Equipment> equipments) {
+        return equipments.get(rand.nextInt(equipments.size()));
     }
 
 }
